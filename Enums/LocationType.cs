dc66143d9b7c652679b691ccec3aeb13393ce488
haibeu2901 <beu2901@gmail.com>
2025-05-09 23:13:14 +0700

    enum location type

---
+++ Enums/LocationType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HunterRPG.Enums
+{
+    public enum LocationType
+    {
+        Forest,
+        River,
+        Cave,
+        Clearing,
+        Camp
+    }
+}
