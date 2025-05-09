18011d2f62537bc1ba62e6be0de41101a39f45f1
haibeu2901 <beu2901@gmail.com>
2025-05-09 23:14:00 +0700

    enum weapon type

---
+++ Enums/ItemType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HunterRPG.Enums
+{
+    public enum ItemType
+    {
+        Food,
+        Material,
+        Tool,
+        Weapon
+    }
+}
