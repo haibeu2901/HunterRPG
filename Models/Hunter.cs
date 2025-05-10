77d1861d4f37fc8502ad3a951878ac0e5b57366d
haibeu2901 <beu2901@gmail.com>
2025-05-10 15:32:27 +0700

    hunter class

---
+++ Models/Hunter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HunterRPG.Models
+{
+    internal class Hunter
+    {
+        public string Name { get; set; }
+        public int Health { get; set; }
+        public int Energy { get; set; }
+        public int Hunger { get; set; }
+
+    }
+}
