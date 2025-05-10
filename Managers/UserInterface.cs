aa7c0ef42bb0887f23b8e149ee2ceff7c9c2b364
haibeu2901 <beu2901@gmail.com>
2025-05-10 23:51:58 +0700

    ui class

---
+++ Managers/UserInterface.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HunterRPG.Managers
+{
+    internal class UserInterface
+    {
+        public static string GetInput()
+        {
+            Console.Write("> ");
+            return Console.ReadLine() ?? string.Empty;
+        }
+
+        public static void DisplayTilte(string title)
+        {
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(new string('=', 50));
+            Console.WriteLine(title.PadLeft(25 + title.Length / 2));
+            Console.WriteLine(new string('=', 50));
+            Console.ResetColor();
+        }
+
+        public static void DisplayMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
