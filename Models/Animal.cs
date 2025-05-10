068ca8555d50affb36e4bae9b169cd7e7fd30a34
haibeu2901 <beu2901@gmail.com>
2025-05-10 15:37:24 +0700

    animal constructor

--- Models/Animal.cs
+++ Models/Animal.cs
@@ -3,10 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using HunterRPG.Enums;
 
 namespace HunterRPG.Models
 {
     internal class Animal
     {
+        public string Name { get; set; }
+        public AnimalType Type { get; set; }
+        public int Health { get; set; }
+        public int Damage { get; set; }
+        public List<Item> Drops { get; set; }
+
+        public Animal(string name, AnimalType type, int health, int damage)
+        {
+            Name = name;
+            Type = type;
+            Health = health;
+            Damage = damage;
+            Drops = new List<Item>();
+
+        }
     }
 }
