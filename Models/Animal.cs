c1092760adb2e790c4eac7920bf60f51be587dd9
haibeu2901 <beu2901@gmail.com>
2025-05-10 23:48:19 +0700

    adding several methods of animal

--- Models/Animal.cs
+++ Models/Animal.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using HunterRPG.Enums;
+using HunterRPG.Utils;
 
 namespace HunterRPG.Models
 {
@@ -22,6 +23,55 @@ namespace HunterRPG.Models
             Health = health;
             Damage = damage;
             Drops = new List<Item>();
+            GenerateDrops();
+        }
+
+        private void GenerateDrops()
+        {
+            switch (Type)
+            {
+                case AnimalType.Deer:
+                    Drops.Add(new Item("Venison", ItemType.Food, 30));
+                    if (RandomGenerator.GetRandomNumber(1, 10) > 5)
+                    {
+                        Drops.Add(new Item("Deer Hide", ItemType.Material, 10));
+                    }
+                    break;
+                case AnimalType.Rabbit:
+                    Drops.Add(new Item("Rabbit Meat", ItemType.Food, 15));
+                    if (RandomGenerator.GetRandomNumber(1, 10) > 7)
+                    {
+                        Drops.Add(new Item("Rabbit Fur", ItemType.Material, 5));
+                    }
+                    break;
+                case AnimalType.Wolf:
+                    Drops.Add(new Item("Wolf Meat", ItemType.Food, 20));
+                    if (RandomGenerator.GetRandomNumber(1, 10) > 6)
+                    {
+                        Drops.Add(new Item("Wolf Pelt", ItemType.Material, 15));
+                    }
+                    break;
+                case AnimalType.Bear:
+                    Drops.Add(new Item("Bear Meat", ItemType.Food, 40));
+                    if (RandomGenerator.GetRandomNumber(1, 10) > 4)
+                    {
+                        Drops.Add(new Item("Bear Hide", ItemType.Material, 25));
+                    }
+                    break;
+
+            }
+        }
+
+        public bool IsAlive() => Health > 0;
+
+        public int Attack()
+        {
+            return RandomGenerator.GetRandomNumber(0, Damage);
+        }
+
+        public void TakeDamage(int damage)
+        {
+            Health = Math.Max(Health - damage, 0);
 
         }
     }
