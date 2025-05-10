1a8e48340e3d1c40181f0fa150ae7dc221029fda
haibeu2901 <beu2901@gmail.com>
2025-05-11 00:01:36 +0700

    add methods for locations (animals, items, and others location's direction)

--- Models/Location.cs
+++ Models/Location.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using HunterRPG.Enums;
+using HunterRPG.Managers;
 
 namespace HunterRPG.Models
 {
@@ -24,6 +25,61 @@ namespace HunterRPG.Models
             Animals = new List<Animal>();
             Items = new List<Item>();
             Connections = new Dictionary<string, Location>();
+        }
+
+        public void AddConnection(string name, Location location)
+        {
+            Connections[name] = location;
+        }
+
+        public void AddAnimal(Animal animal)
+        {
+            Animals.Add(animal);
+        }
+
+        public void AddItem(Item item)
+        {
+            Items.Add(item);
+        }
+
+        public void RemoveAnimal(Animal animal)
+        {
+            Animals.Remove(animal);
+        }
+
+        public void RemoveItem(Item item)
+        {
+            Items.Remove(item);
+        }
+
+        public void DisplayDetails()
+        {
+            UserInterface.DisplayMessage($"Location: {Name}");
+            UserInterface.DisplayMessage(Description);
+
+            if (Animals.Count > 0)
+            {
+                UserInterface.DisplayMessage("Animals:");
+                foreach (var animal in Animals)
+                {
+                    UserInterface.DisplayMessage($"  {animal.Name} (Health: {animal.Health})");
+                }
+            }
+
+            if (Items.Count > 0)
+            {
+                UserInterface.DisplayMessage("Items:");
+                foreach (var item in Items)
+                {
+                    UserInterface.DisplayMessage($"  {item.Name}");
+                }
+            }
+
+            UserInterface.DisplayMessage("Connections:");
+            foreach (var connection in Connections)
+            {
+                UserInterface.DisplayMessage($"  {connection.Key}: {connection.Value.Name}");
+            }
         }
     }
 }
