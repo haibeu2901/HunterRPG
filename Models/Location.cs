57b429c6416511928dd3ed5b230a420483468b0a
haibeu2901 <beu2901@gmail.com>
2025-05-10 15:39:59 +0700

    location contructor

--- Models/Location.cs
+++ Models/Location.cs
@@ -3,11 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using HunterRPG.Enums;
 
 namespace HunterRPG.Models
 {
     internal class Location
     {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public LocationType Type { get; set; }
+        public List<Animal> Animals { get; set; }
+        public List<Item> Items { get; set; }
+        public Dictionary<string, Location> Connections { get; set; }
 
+        public Location(string name, string description, LocationType type)
+        {
+            Name = name;
+            Description = description;
+            Type = type;
+            Animals = new List<Animal>();
+            Items = new List<Item>();
+            Connections = new Dictionary<string, Location>();
+        }
     }
 }
