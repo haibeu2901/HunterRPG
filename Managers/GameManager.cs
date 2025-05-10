aa8458af5145de8cb9f1faf3be796e3e0a4b7523
haibeu2901 <beu2901@gmail.com>
2025-05-11 00:23:29 +0700

    game constructor

---
+++ Managers/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HunterRPG.Models;
+
+namespace HunterRPG.Managers
+{
+    internal class GameManager
+    {
+        private Hunter hunter;
+        private List<Location> world;
+        private bool isRunning;
+        private int day;
+
+        public GameManager()
+        {
+            world = new List<Location>();
+            isRunning = false;
+            day = 1;
+        }
+    }
+}
