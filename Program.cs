a1c675e7a5dd5b19fd492746f89bced6f15c2091
haibeu2901 <beu2901@gmail.com>
2025-05-11 01:10:37 +0700

    final finish

--- Program.cs
+++ Program.cs
@@ -1,10 +1,13 @@
-﻿namespace HunterRPG
+﻿using HunterRPG.Managers;
+
+namespace HunterRPG
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-
+            var game = new GameManager();
+            game.Start();
         }
     }
 }
