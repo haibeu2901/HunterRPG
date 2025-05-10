289fe441a1de7980680948db678faf731deac5ad
haibeu2901 <beu2901@gmail.com>
2025-05-10 15:33:38 +0700

    item class

---
+++ Models/Item.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HunterRPG.Enums;
+
+namespace HunterRPG.Models
+{
+    internal class Item
+    {
+        public string Name { get; set; }
+        public ItemType Type { get; set; }
+        public int Value { get; set; }
+
+        public Item(string name, ItemType type, int value)
+        {
+            Name = name;
+            Type = type;
+            Value = value;
+        }
+    }
+}
