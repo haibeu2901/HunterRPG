30f12e76f42dee6337d2290e77362b938e380268
haibeu2901 <beu2901@gmail.com>
2025-05-11 00:36:30 +0700

    create game function

--- Managers/GameManager.cs
+++ Managers/GameManager.cs
@@ -28,8 +28,31 @@ namespace HunterRPG.Managers
             GameLoop();
         }
 
-        
+        private void InitializeGame()
+        {
+            UserInterface.DisplayTitle("Hunter RPG");
+            UserInterface.DisplayMessage("Welcome to Hunter RPG!");
+            UserInterface.DisplayMessage("You are a hunter trying to survive in the wilderness.");
 
-        
+            // Create player
+            UserInterface.DisplayMessage("Enter your name:");
+            string hunterName = UserInterface.GetInput();
+            hunter = new Hunter(hunterName);
+
+            // Create world
+            InitializeGameWorld();
+
+            // Give player starting items
+            hunter.AddItem(new Item("Hunting Knife", ItemType.Weapon, 10));
+            hunter.AddItem(new Item("Wooden Bow", ItemType.Weapon, 20));
+            hunter.AddItem(new Item("Jerky", ItemType.Food, 15));
+
+            // Set player location
+            hunter.CurrentLocation = gameWorld.Find(location => location.Type == LocationType.Camp);
+
+            isRunning = true;
+        }
+
+
     }
 }
